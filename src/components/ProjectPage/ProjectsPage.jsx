8f5b3fbd7f1aae717f5d87cbdba97de31cf17791
avishek subedi <avisheksubedi05@gmail.com>
2025-05-28 22:47:08 +0545

    projects list-added

--- src/components/ProjectPage/ProjectsPage.jsx
+++ src/components/ProjectPage/ProjectsPage.jsx
@@ -1,50 +1,33 @@
 import React from "react";
 import "./ProjectsPage.css";
 import Project from "../Projects/Project";
+import ProjectsData from "../../data/ProjectsData";
 
-const projectData = [
-    {
-        title: "Portfolio Website",
-        description: "My personal website built with React showcasing my skills and projects.",
-        image: "src/assets/images/portfoli image.png",
-        link: "",
-    },
-    {
-        title: "Weather App",
-        description: "Fetches real-time weather data using OpenWeatherMap API.",
-        image: "src/assets/images/weather app.png",
-        link: "https://weatherapp.com",
-    },
-    {
-        title: "Task Manager",
-        description: "To-do list with CRUD operations and localStorage. It is a good app With showcasing my Java script skills.",
-        image: "src/assets/images/todo list.jpeg",
-        link: "https://taskmanager.com",
-    },
-];
+
 
 const ProjectsPage = () => {
     return (
         <>
-        <section id="project"></section>
-        <section className="projects-page">
-            <h2>My Projects</h2>
+            <section id="project"></section>
+            <section className="projects-page">
+                <h2>My Projects</h2>
+                <p className="project-intro">
+                    Here are the projects built by me, showcasing my skills through small applications.
+                </p>
+                <div className="projects-container">
+                    {ProjectsData.map((project, index) => (
+                        <Project
+                            key={index}
+                            title={project.title}
+                            description={project.description}
+                            image={project.image}
+                            link={project.link}
+                        />
+                    ))}
+                </div>
+            </section>
+          
 
-            <p className="project-intro"> Here are the Projects Build My me Showcasing my skills and using them to build smalls projects.</p>
-            <div className="projects-container">
-                {projectData.map((project, index) => (
-                    <Project
-                        key={index}
-                        title={project.title}
-                        description={project.description}
-                        image={project.image}
-                        link={project.link}
-                    />
-
-
-                ))}
-            </div>
-        </section>
         </>
     );
 };
---
+++ src/data/ProjectsData.js
@@ -0,0 +1,47 @@
+const ProjectsData = [
+  {
+    title: "Portfolio Website",
+    description: "My personal website built with React showcasing my skills and projects.",
+    image: "src/assets/images/portfoli image.png",
+    link: ""
+  },
+  {
+    title: "Weather App",
+    description: "Fetches real-time weather data using OpenWeatherMap API.",
+    image: "src/assets/images/weather app.png",
+    link: "https://github.com/avishek-subedi05/js_projects/tree/main/jsp1/weather%20app"
+  },
+  {
+    title: "Task Manager",
+    description: "To-do list with CRUD operations and localStorage. It is a good app showcasing my JavaScript skills.",
+    image: "src/assets/images/todo-list.png",
+    link: "https://github.com/avishek-subedi05/js_projects/tree/main/jsp1/todolist"
+  }
+  ,
+   {
+    title: "Digital Clock",
+    description: "Digital clock is short app developed using js. It is a good app showcasing my JavaScript skills.",
+    image: "src/assets/images/clock.png",
+    link: "https://github.com/avishek-subedi05/js_projects/tree/main/jsp1/clock%20program"
+  },
+   {
+    title: "Rock Paper Scissor",
+    description: " As a basic in js, I buils a rock paper scissor game.Where a playe play a game with computer and a score is shown in screen.",
+    image: "src/assets/images/rock-paper.png",
+    link: "https://github.com/avishek-subedi05/js_projects/tree/main/jsp1/rock%20paper%20scissor%20game"
+  },
+   {
+    title: "Stop-Watch(milli-sec)",
+    description: "Just digital stop watch.",
+    image: "src/assets/images/stop-watch.png",
+    link: "https://github.com/avishek-subedi05/js_projects/tree/main/jsp1/stopwatch"
+  },
+   {
+    title: "Calculator",
+    description: "What Do you think about this.",
+    image: "src/assets/images/calculator.png",
+    link: "https://github.com/avishek-subedi05/js_projects/tree/main/jsp1/Calculator"
+  }
+];
+
+export default ProjectsData;
